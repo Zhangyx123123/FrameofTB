f9adf16f429e975f8fa057976229870938ab41c6
zhangyuxuan <2236720696@qq.com>
2017-12-21 14:26:51 +0800

    this is a commit

---
+++ src/pages/detail/detail.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-about',
+  templateUrl: 'detail.html'
+})
+export class DetailPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
